72818c36a95a44b7aad4f9b89f57f4fa8ef7ecaa
fhur <fernando@authy.com>
2015-08-23 22:21:25 +0200

    Bumped version number and added project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject typewriter "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject typewriter "0.2.0"
+  :description "TypeWriter: java code generation utils"
+  :url "http://github.com/fhur/typewriter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
